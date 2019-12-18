99718354ed4ed6b66dc546bedc6e1e3b31aa2ca0
Chirag <pawarcrg@live.com>
2019-12-18 10:08:09 +0530

    db changes

--- public/config/Configuration.js
+++ public/config/Configuration.js
@@ -1,5 +1,5 @@
 const Mongoose = require('mongoose');
-const Url = 'mongodb+srv://ringfuze:MYdzca1kIas50Y8Q@ringfuze-dev-gq9is.mongodb.net/WherebyDb';
+const Url = 'mongodb+srv://Chiragpwr:Chiragpwr?007@cluster0-7a8as.mongodb.net/WherebyDb';
 //"mongodb://localhost:27017/Testdb";
 // mongodb+srv://Chiragpwr:<Chiragpwr?007>@cluster0-7a8as.mongodb.net/WherebyDb
 const mailCredential = {
